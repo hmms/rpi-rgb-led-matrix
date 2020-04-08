6cedb4d42066906daaf71590b44d5e22af356038
MurliS <murlidhar.shenoy@gmail.com>
2020-04-07 19:12:07 -0700

    runtext.py updated to clock

--- bindings/python/samples/runtext.py
+++ bindings/python/samples/runtext.py
@@ -3,7 +3,8 @@
 from samplebase import SampleBase
 from rgbmatrix import graphics
 import time
-
+from datetime import datetime
+import math
 
 class RunText(SampleBase):
     def __init__(self, *args, **kwargs):
@@ -14,18 +15,27 @@ class RunText(SampleBase):
         offscreen_canvas = self.matrix.CreateFrameCanvas()
         font = graphics.Font()
         font.LoadFont("../../../fonts/7x13.bdf")
-        textColor = graphics.Color(255, 255, 0)
         pos = offscreen_canvas.width
         my_text = self.args.text
-
+	frequency = .1
+	i = 0
         while True:
-            offscreen_canvas.Clear()
-            len = graphics.DrawText(offscreen_canvas, font, pos, 10, textColor, my_text)
-            pos -= 1
-            if (pos + len < 0):
-                pos = offscreen_canvas.width
+	    if(i < 32):
+	        i = i + 1
+	    else:
+		i = 0
+	    red   = math.sin(frequency*i + 0) * 63 + 110
+            green = math.sin(frequency*i + 2) * 63 + 110
+            blue  = math.sin(frequency*i + 4) * 63 + 110
 
-            time.sleep(0.05)
+	    textColor = graphics.Color(red, green, blue)
+            now = datetime.now()
+	    my_text = now.strftime("%H:%M:%S")
+            offscreen_canvas.Clear()
+            len = graphics.DrawText(offscreen_canvas, font, pos, 32, textColor, my_text)
+            pos = 4
+
+	    time.sleep(0.5)
             offscreen_canvas = self.matrix.SwapOnVSync(offscreen_canvas)
 
 
